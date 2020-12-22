a3add786572787ef454fe29ae4f8e8bde366ddc1
akylik <a_kylik@ukr.net>
2020-12-22 17:00:54 +0200

    fixed main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
+let audio;
 function playSound(e) {
-  const audio = document.querySelector(`audio[data-letter="${e.code}"]`);
-  if (!audio) return;
-  audio.currentTime = 0;
-  audio.play();
+    if(audio) audio.pause();
+    audio = document.querySelector(`audio[data-letter="${e.code}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
 }
-
 window.addEventListener("keydown", playSound);
